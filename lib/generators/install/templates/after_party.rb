592fba42bc5b7afd5b93c6f4167725fd4bc4a3a3
Manu Campos <manu@invisibles.cc>
2016-07-29 12:57:09 +0200

    Removes more rails dependencies.

--- lib/generators/install/templates/after_party.rb
+++ lib/generators/install/templates/after_party.rb
@@ -1,7 +1,3 @@
 AfterParty.setup do |config|
-  # ==> ORM configuration
-  # Load and configure the ORM. Supports :active_record (default) and
-  # :mongoid (bson_ext recommended) by default. Other ORMs may be
-  # available as additional gems.
-  require "after_party/<%= orm_name %>.rb"
+  require 'after_party/active_record.rb'
 end
